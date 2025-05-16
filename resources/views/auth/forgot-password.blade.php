1ceb74eb9a72456e631c7e7180bb25244f7f4404
Fachri96 <149947273+MFachriA96@users.noreply.github.com>
2025-05-16 12:46:14 +0700

    Create forgot-password.blade.php

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,8 @@
+@extends('auth.layouts')
+
+@section('title', 'Forgot Password - NutriTrack')
+
+@section('content')
+    <div id="forgot-password-app"></div>
+    @vite('resources/js/forgot-password.js')
+@endsection 
